4cd1948ef3f638464f356d50f88bf1b1555f91d4
Genichi Ishii <gishii507@gmail.com>
2025-02-23 12:14:22 +0900

    ESMモジュールサポートのためにインポート文を変更し、package.jsonにモジュールタイプとスタートスクリプトを追加

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const functions = require('@google-cloud/functions-framework');
+import functions from '@google-cloud/functions-framework';
 
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
