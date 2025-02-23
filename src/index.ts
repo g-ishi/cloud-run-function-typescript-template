a74e3491283304fe0831b112127cbba1ae080e73
Genichi Ishii <gishii507@gmail.com>
2025-02-23 12:22:27 +0900

    HTTP関数を追加し、TypeScriptの設定を導入。package.jsonにビルドおよびデプロイスクリプトを追加。

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import * as functions from '@google-cloud/functions-framework';
+import { Request, Response } from 'express';
+
+functions.http('helloGET', (req: Request, res: Response) => {
+  const num: number = 123;
+  console.log(num);
+  res.send('Hello World!');
+});
